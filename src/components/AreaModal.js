14352212fc6b3f1655f6ae4fd3cf35c2bcdedca4
Karina Miernik <karina.miernik@gmail.com>
2022-07-05 08:04:16 +0200

    add comment

--- src/components/AreaModal.js
+++ src/components/AreaModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// modal for activitiess
 export default function AreaModal({title, image, text, index}) {
   return (
 <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" 
@@ -14,6 +15,7 @@ id={`exampleModalCenter${index}`} tabindex="-1" aria-labelledby="exampleModalCen
           class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
           data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+      
       <div class="modal-body relative p-4 flex flex-row justify-around flex-wrap w-full">
         <div class="flex flex-col w-[40%]">
         <img src={image} class="max-w-full max-h-[300px] m-auto h-auto" alt="..." />
