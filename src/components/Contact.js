55d24f46b26f5ffe6504df3511410d1504fd0564
karina <karina.miernik@gmail.com>
2024-08-26 07:56:30 +0200

    remove form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,27 @@
 import React from "react";
-import Form from "./Form";
 import SectionTitle from "./SectionTitle";
 import MyMap from "../Maps/MyMap";
 import { BsTelephoneFill } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 import { SiGooglemaps } from "react-icons/si";
 import { SiOpenstreetmap } from "react-icons/si";
+import { NavLink } from "react-router-dom";
 export default function Contact() {
     return (
         <>
             {" "}
             <div className="w-full min-h-[90vh] mx-auto my-26 flex flex-col max-w-[1440px] ">
                 <SectionTitle text="Skontaktuj się z nami!" />
-                <div className="w-[90%] m-auto flex flex-row md:flex-row flex-wrap justify-around items-center mt-16 gap-8 lg:gap-0 mb-4">
+                <div className="w-[90%] m-auto flex flex-row md:flex-row flex-wrap justify-center items-start mt-16 gap-8 lg:gap-4 mb-4">
                     <div className="font-lato font-light flex flex-col justify-start w-full md:w-1/2 lg:w-[25%] gap-4">
                         <span className="flex items-center gap-4 transition duration-300 ease-in-out hover:text-c-primary">
                             <BsTelephoneFill /> +48 608 458 768
                         </span>
                         <span className="flex items-center gap-4 transition duration-300 ease-in-out hover:text-c-primary">
                             <AiOutlineMail />{" "}
-                            <a href="mailto:dartojab@gmail.com">
+                            <NavLink to="mailto:dartojab@gmail.com">
                                 dartojab@gmail.com
-                            </a>
+                            </NavLink>
                         </span>
                         <span className="flex items-center gap-4 transition duration-300 ease-in-out hover:text-c-primary">
                             <SiGooglemaps />
@@ -34,8 +34,7 @@ export default function Contact() {
                             Jabłonka 84, 13-100 Natać Wielka
                         </span>
                     </div>
-                    <Form />
-                    <div className="h-[300px] lg:h-[400px] w-full lg:w-[35%] m-auto">
+                    <div className="h-[300px] lg:h-[400px] w-full lg:w-[35%]">
                         <MyMap />
                     </div>
                 </div>
